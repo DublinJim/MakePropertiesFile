1dad5e31e4848928c089a82bf75953ece4ad42ae
DublinJim <59180380+DublinJim@users.noreply.github.com>
2023-09-15 12:10:51 -0400

    setup working

--- src/main/java/com/example/makepropertiesfile/Controller.java
+++ src/main/java/com/example/makepropertiesfile/Controller.java
@@ -5,7 +5,10 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.net.URL;
+import java.util.Properties;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
@@ -20,6 +23,25 @@ public class Controller implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        
+        Properties properties = new Properties();
+String fileName ="C:\\Users\\james.keogh\\IdeaProjects\\MakePropertiesFile\\src\\main\\java\\com\\example\\makepropertiesfile\\app.properties";
+        try {
+            FileInputStream fileInputStream = new FileInputStream(fileName);
+            properties.load(fileInputStream);
+            fileInputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String title = properties.getProperty("app.title");
+        String version = properties.getProperty("app.version");
+        String author = properties.getProperty("app.author");
+        String controller = properties.getProperty("app.controller");
+
+        txt2.setText(title);
+        txt1.setText(version);
+        txt0.setText(author);
+        txt3.setText(controller);
+
     }
 }
