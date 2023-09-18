48f35e0c8f4991ff8100d926458a6692abe8bdb2
DublinJim <dublinjim.jk@gmail.com>
2023-09-18 10:41:20 -0400

    new css

--- src/main/java/com/example/makepropertiesfile/Controller.java
+++ src/main/java/com/example/makepropertiesfile/Controller.java
@@ -1,12 +1,12 @@
 package com.example.makepropertiesfile;
 
 import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
@@ -22,27 +22,12 @@ public class Controller implements Initializable {
     public Text txt5;
     public Label Lbl1;
     public AnchorPane rootPaneMain;
+    public CheckBox chkBoxDarkMode;
+    public Text txtDarkModeFlag;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        Properties properties = new Properties();
-
-        properties.setProperty("author","James M. Keogh");
-        properties.setProperty("title","Make properties");
-        properties.setProperty("controller","Controller.java");
-        properties.setProperty("company","ADGA");
-        properties.setProperty("timeMade","2023");
-        // Add key-value pairs
-
-        try {
-            FileOutputStream fileOutputStream = new FileOutputStream("app.properties");
-            properties.store(fileOutputStream, "Application Properties");
-            fileOutputStream.close();
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        Properties properties = getProperties();
 
 
         try {
@@ -68,6 +53,36 @@ public class Controller implements Initializable {
         txt3.setText(controller);
         txt4.setText(company);
         txt5.setText(timeMade);
+
+    }
+
+    private static Properties getProperties() {
+        Properties properties = new Properties();
+
+        properties.setProperty("author","James M. Keogh");
+        properties.setProperty("title","Make properties");
+        properties.setProperty("controller","Controller.java");
+        properties.setProperty("company","ADGA");
+        properties.setProperty("timeMade","2023");
+        properties.setProperty("darkMode","false");
+        // Add key-value pairs
+
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream("app.properties");
+            properties.store(fileOutputStream, "Application Properties");
+            fileOutputStream.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return properties;
+    }
+
+    public void cssDarkMode() {
+        if (chkBoxDarkMode.isSelected()) {
+            rootPaneMain.getStyleClass().add("darkMode");
+        }else{
+            rootPaneMain.getStyleClass().remove("darkMode");
+        }
 
     }
 }
